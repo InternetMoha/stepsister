c834f3835097340f84024406fa8459dd3dcf5147
Moha Goat <internetmohaa@gmail.com>
2022-04-16 23:51:18 +0300

    it's my website now.

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,12 +1,12 @@
- const iplocation = "https://api.ipgeolocation.io/ipgeo?apiKey=b047f3afe9fa44dca7f0c00a17443003";
+const iplocation = "https://api.ipgeolocation.io/ipgeo?apiKey=b047f3afe9fa44dca7f0c00a17443003";
 
-   $.getJSON(iplocation, (data) => {
+$.getJSON(iplocation, (data) => {
 
-     const ip = data.ip;
-     const country = data.country_name ? data.country_name : 'your country';
+  const ip = data.ip;
+  const country = data.country_name ? data.country_name : 'your country';
 
-     $("p#ip").html(`<p class="top-right">${ip} ${country}</p>`);
-   })
+  $("p#ip").html(`<p class="top-right">${ip} ${country}</p>`);
+})
 
 titleChanger = (text, delay) => {
   if (!text) return;
@@ -21,7 +21,7 @@ titleChanger = (text, delay) => {
   }, delay);
 };
 
-titleChanger(["normie", "synthi#0001", "if you can see this say hi to me", "discord.fmhy.tk", "make information free again", "#otters4life"], 2000);
+titleChanger(["Cent", "Cent#2220", "if you see this send nudes", "stepmommy.xyz/fmhy", "touch grass today", "#TB4L"], 2000);
 
 cheet('a w e s o m e', function () {
   let audio = new Audio("../assets/others/gecs.mp3")
@@ -31,36 +31,36 @@ cheet('a w e s o m e', function () {
 
 function thoughts() {
   const element = document.querySelector('#content')
-    element.text = 'if you can see this you are awesome :3'
+  element.text = 'if you can see this you are awesome :3 - can you stfu im trying to fix this bad js -cent.'
 
-    fetch('https://www.reddit.com/r/Showerthoughts.json?raw_json=1&amp;limit=10')
-      .then(res => res.text())
-      .then(text => {
-        if (!text) {
-          element.text = 'what are you using? internet explorer? it didnt let me get reddit posts >:('
-          return
-        }
+  fetch('https://www.reddit.com/r/Showerthoughts.json?raw_json=1&amp;limit=10')
+    .then(res => res.text())
+    .then(text => {
+      if (!text) {
+        element.text = 'what are you using? internet explorer? it didnt let me get reddit posts >:('
+        return
+      }
 
-        const body = JSON.parse(text)
-        const posts = body.data.children.filter(p => !p.data.pinned && !p.data.stickied)
-        const post = posts[Math.floor(Math.random() * posts.length)]
+      const body = JSON.parse(text)
+      const posts = body.data.children.filter(p => !p.data.pinned && !p.data.stickied)
+      const post = posts[Math.floor(Math.random() * posts.length)]
 
-        let title = post.data.title.trim()
+      let title = post.data.title.trim()
 
-        if (!title.endsWith('.')) title += '.'
+      if (!title.endsWith('.')) title += '.'
 
-        title = title.toLowerCase()
-        title += ' '
-        element.textContent = title
+      title = title.toLowerCase()
+      title += ' '
+      element.textContent = title
 
-        const attr = document.createElement('a')
-        attr.setAttribute('href', post.data.url)
-        attr.setAttribute('target', '_blank')
-        attr.innerText = post.data.author.toLowerCase()
-        element.appendChild(attr)
-      })
-      .catch(e => {
-        console.error(e)
-        element.textContent = 'something went wrong :('
-      })
+      const attr = document.createElement('a')
+      attr.setAttribute('href', post.data.url)
+      attr.setAttribute('target', '_blank')
+      attr.innerText = post.data.author.toLowerCase()
+      element.appendChild(attr)
+    })
+    .catch(e => {
+      console.error(e)
+      element.textContent = 'something went wrong :('
+    })
 }
