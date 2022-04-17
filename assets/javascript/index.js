02dc5541058672b6739556197007ca40640c8e36
Moha Goat <internetmohaa@gmail.com>
2022-04-17 03:45:20 +0300

    pray it works

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -27,6 +27,7 @@ cheet('c e n t', function () {
   let audio = new Audio("../assets/others/gecs.mp3")
   audio.play()
   document.getElementById("logo").src = "../assets/others/lol.gif";
+  document.getElementById("download").src = "/assets/others/lol.gif"
 });
 
 function thoughts() {
